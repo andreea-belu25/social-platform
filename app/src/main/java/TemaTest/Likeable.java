33fa7baf13005abfc990d50c97dcd7383bf8da32
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 20:27:54 +0300

    Update Likeable.java

--- app/src/main/java/TemaTest/Likeable.java
+++ app/src/main/java/TemaTest/Likeable.java
@@ -1,10 +1,9 @@
 package TemaTest;
 
-//  definesc fct comune din clasele Comentariu si Postare
+// Define common functions from Comment and Post classes
 public interface Likeable {
-    public boolean isAlreadyLiked (String username);
-    public void addUsernameThatLiked (String username);
-    public boolean isUnliked (String username);
+    public boolean isAlreadyLiked(String username);
+    public void addUsernameThatLiked(String username);
+    public boolean isUnliked(String username);
     public void deleteUsernameThatLiked(String username);
-
 }
